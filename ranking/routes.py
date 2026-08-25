88d4bea4f23d97cb789f8337defcd82673dc1b2a
kylekim <kylekim0868@gmail.com>
2026-08-25 19:50:26 +0900

    랭킹 기능 구현

--- ranking/routes.py
+++ ranking/routes.py
@@ -18,9 +18,25 @@ ranking_bp = Blueprint("ranking", __name__)
 def ranking():
     date = get_today_date()
     # TODO(팀원 B): 닉네임 조인 + 내 순위 강조
+    # 정렬 우선순위: 점수 > 힌트 사용횟수 > 풀이 완료 시각
     rows = list(db.solves.find({"date": date, "status": "solved"})
-                .sort([("score", -1), ("solved_at", 1)]))
-    return render_template("ranking/ranking.html", date=date, rows=rows)
+                .sort([("score", -1), ("hints_used", 1),("solved_at", 1)]))
+    # db에서 rows데이터에 들어있는 정보를 통해 닉네임을 찾고 rows에 다시 닉네임을 주입시키는 로직
+    # 순위를 나타낼 때 예외 사항 
+    # 1) 점수 동일 2) 힌트 사용횟수 동일 3) 풀이완료시각 동일 조건일 경우 -> 공동 순위 부여
+    rank_rows = []
+    for idx, row in enumerate(rows, start=1):
+        user = db.users.find_one({"_id": row["user_id"]})
+        rank_rows.append({
+            "rank": idx,
+            "nickname": user["nickname"] if user else "알 수 없음",
+            "score": row["score"],
+            "solved_at": row["solved_at"],
+            "hints_used": row["hints_used"],
+            "is_me": row["user_id"] == g.user_id
+        })
+
+    return render_template("ranking/ranking.html", date=date, rows=rank_rows)
 
 
 @ranking_bp.route("/history")
